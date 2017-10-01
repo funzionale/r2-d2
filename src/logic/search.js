8e518c88352dff365573aa2efe766213355e7d50
A.R. Maged <abdulrahman.maged95@gmail.com>
2017-10-01 21:07:12 +0200

    :sparkles: Implement general search function

---
+++ src/logic/search.js
@@ -0,0 +1,57 @@
+/** @flow */
+
+/** @TODO: Define */
+type Action = {};
+
+/** @TODO: Define */
+type State = {};
+
+type Node = {
+  state: State,
+  parent: Node,
+  operator: Action,
+  depth: number,
+  pathCost: number,
+};
+
+type Problem = {
+  operators: Array<Action>,
+  initialState: State,
+  stateSpace: Array<State>,
+  goalTest: State => boolean,
+  pathCost: (Array<Action>) => number,
+};
+
+const initialState: Problem => State = problem => problem.initialState;
+
+/** @TODO: Implement */
+const expand: (Node, Array<Action>) => Array<Node> = () => {};
+
+/** @TODO: Implement */
+const makeNode: State => Node = state => {};
+
+/** @TODO: Implement */
+const makeQueue: Node => Array<Node> = () => {};
+
+const goalTest: Problem => State => boolean = problem => problem.goalTest;
+
+const state: Node => State = node => node.state;
+
+const operation: Problem => Array<Action> = problem => problem.operators;
+
+export const generalSearch = (
+  problem: Problem,
+  queueingFunc: (Array<Node>, Array<Node>) => Array<Node>
+) => {
+  let nodes = makeQueue(makeNode(initialState(problem)));
+  while (nodes.length > 0) {
+    const [node] = nodes.splice(0, 1);
+    if (goalTest(problem)(state(node))) {
+      return node;
+    }
+    nodes = queueingFunc(nodes, expand(node, operation(problem)));
+  }
+  return [];
+};
+
+/** @TODO: Implement queueing functions */
