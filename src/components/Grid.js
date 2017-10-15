88b717a00fa1bf5e60259869fdc01519dedbb20e
Ahmed Elhanafy <ahmed.elhanafy95@gmail.com>
2017-10-15 22:35:01 +0200

    :lipstick: Finish up the basic grid UI

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,21 +1,93 @@
+/* @flow */
+
 import React from 'react';
 import { connect } from 'react-redux';
+import type { Cell, Item } from '../flow';
 
-/** @TODO: Do your magic */
-const Grid = ({ grid }) => {
-  const cells = Math.sqrt(grid.length);
-  return grid.map(grid => (
+const Pad = () => <div>'PAD'</div>;
+const Rock = () => <img className="rock" src="./rock.png" alt="Rock" />;
+const Teleportal = () => <div>'TELEPORTAL'</div>;
+const Obstacle = () => <div>'OBSTACLE'</div>;
+const R2D2 = () => <img className="r2d2" src="./R2D2.svg" alt="Robot" />;
+
+const Grid = ({ grid }: { grid: Array<Cell> }) => {
+  const numOfRows = Math.sqrt(grid.length);
+  const widthAndHeight = Math.floor(100 / numOfRows);
+  return grid.map(gridCell => [
     <div
       style={{
-        background: 'rgba(0,0,0,0.08)',
-        margin: 2,
-        width: `${100 / cells - 0.67}%`,
-        height: `${100 / cells - 0.67}%`,
-        borderRadius: 9,
-        boxShadow: '1px 2px 2px 0 rgba(0,0,0,0.16)',
+        width: `${widthAndHeight}%`,
+        maxWidth: `${widthAndHeight}%`,
+        height: `${widthAndHeight}%`,
+        maxHeight: `${widthAndHeight}%`,
+        padding: 4,
+        boxSizing: 'border-box',
       }}
-    />
-  ));
+    >
+      <div
+        style={{
+          background: 'rgba(0,0,0,0.08)',
+          borderRadius: 9,
+          boxShadow: '1px 2px 2px 0 rgba(0,0,0,0.16)',
+          height: '100%',
+          width: '100%',
+        }}
+      />
+    </div>,
+    [
+      gridCell.items.map((item: Item) => {
+        const translateX = 600 / numOfRows * gridCell.coordinates.x;
+        const translateY = 600 / numOfRows * gridCell.coordinates.y;
+        const width = `${Math.floor(100 / numOfRows)}%`;
+        const height = `${Math.floor(100 / numOfRows)}%`;
+        let comp;
+        let zIndex = 0;
+        switch (item) {
+          case 'R2D2':
+            comp = <R2D2 />;
+            zIndex = 2;
+            break;
+          case 'TELEPORTAL':
+            comp = <Teleportal />;
+            zIndex = 1;
+            break;
+          case 'OBSTACLE':
+            comp = <Obstacle />;
+            zIndex = 2;
+            break;
+          case 'ROCK':
+            comp = <Rock />;
+            zIndex = 2;
+            break;
+          case 'PAD':
+            comp = <Pad />;
+            zIndex = 1;
+            break;
+          default:
+            return null;
+        }
+        return (
+          <div
+            style={{
+              position: 'absolute',
+              left: 0,
+              top: 0,
+              zIndex,
+              width,
+              height,
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              transform: `translate(${translateX}px, ${translateY}px)`,
+              transition: 'translate 1s ease',
+            }}
+          >
+            {comp}
+          </div>
+        );
+      }),
+    ],
+  ]);
 };
 
 const mapStateToProps = state => ({
