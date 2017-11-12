0dff2c5fa2e0996dbf9196f075502df5ae2aa8d0
A.R. Maged <abdulrahman.maged95@gmail.com>
2017-11-12 16:09:05 +0200

    :sparkles: Implement knowledge base reducer

--- src/redux/reducers/data/index.js
+++ src/redux/reducers/data/index.js
@@ -2,7 +2,9 @@
 
 import { combineReducers } from 'redux';
 import grid from './grid';
+import knowledgeBase from './knowledgeBase';
 
 export default combineReducers({
   grid,
+  knowledgeBase,
 });
