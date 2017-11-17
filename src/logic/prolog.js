ae337e898bff1c3eb72fe8f8f383686a8e0b76ba
A.R. Maged <abdulrahman.maged95@gmail.com>
2017-11-17 13:24:08 +0200

    :hammer: Update transformGridToPrologFacts()'s implementation

--- src/logic/prolog.js
+++ src/logic/prolog.js
@@ -1,13 +1,18 @@
 /** @flow */
+import _ from 'lodash';
 
 import type { Cell } from '../flow';
 
 export const transformGridToPrologFacts: (
   Array<Cell>
 ) => Array<string> = grid =>
-  grid.map(
-    cell =>
-      `cell(${cell.coordinates.x}, ${cell.coordinates.y}, ${JSON.stringify(
-        cell.items
-      ).replace(/"/g, "'")})`
-  );
+  grid
+    .map(
+      cell =>
+        cell.items.map(
+          item =>
+            `${item.toLowerCase()}(${cell.coordinates.x}, ${cell.coordinates
+              .y}, s0).`
+        )[0]
+    )
+    .filter(fact => !_.isUndefined(fact));
